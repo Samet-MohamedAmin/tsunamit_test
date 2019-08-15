caca4ddcb98dfc53a92946d1f3c21597fadcc98f
MohamedAmin Samet <mohamedamin.samet@yandex.com>
2019-08-15 19:13:19 +0100

    add error component. closes #8

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { ErrorComponent } from './error/error.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
@@ -11,11 +12,13 @@ import { HomeComponent } from './home/home.component';
   declarations: [
     NavbarComponent,
     FooterComponent,
+    ErrorComponent,
     HomeComponent
   ],
   exports: [
     NavbarComponent,
     FooterComponent,
+    ErrorComponent,
     HomeComponent
   ]
 })
